3772bb1d1d578dfb489536f6580be101727f27a4
Miron Fritz <mironfritz@icloud.com>
2020-07-21 12:46:18 +0200

    migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,75 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_093856) do
+ActiveRecord::Schema.define(version: 2020_07_21_103243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.boolean "value"
+    t.bigint "user_id", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "career_traits", force: :cascade do |t|
+    t.bigint "career_id", null: false
+    t.bigint "trait_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["career_id"], name: "index_career_traits_on_career_id"
+    t.index ["trait_id"], name: "index_career_traits_on_trait_id"
+  end
+
+  create_table "careers", force: :cascade do |t|
+    t.string "title"
+    t.text "summary"
+    t.text "detailed_description"
+    t.string "social_interaction"
+    t.integer "salary"
+    t.string "entry_barriers"
+    t.string "remote_flexibility"
+    t.string "international_flexibility"
+    t.string "working_hour_flexibility"
+    t.text "next_steps"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "question"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trait_questions", force: :cascade do |t|
+    t.string "answer_value"
+    t.bigint "question_id", null: false
+    t.bigint "trait_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_trait_questions_on_question_id"
+    t.index ["trait_id"], name: "index_trait_questions_on_trait_id"
+  end
+
+  create_table "traits", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_traits", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "trait_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["trait_id"], name: "index_user_traits_on_trait_id"
+    t.index ["user_id"], name: "index_user_traits_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +88,19 @@ ActiveRecord::Schema.define(version: 2020_07_21_093856) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "career_id"
+    t.index ["career_id"], name: "index_users_on_career_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
+  add_foreign_key "career_traits", "careers"
+  add_foreign_key "career_traits", "traits"
+  add_foreign_key "trait_questions", "questions"
+  add_foreign_key "trait_questions", "traits"
+  add_foreign_key "user_traits", "traits"
+  add_foreign_key "user_traits", "users"
+  add_foreign_key "users", "careers"
 end
