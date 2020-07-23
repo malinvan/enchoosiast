d32c2c530f66fca6353ff86802ed9a8630b48761
Miron Fritz <mironfritz@icloud.com>
2020-07-23 19:23:11 +0200

    basic style for career show

--- app/models/career.rb
+++ app/models/career.rb
@@ -1,5 +1,5 @@
 class Career < ApplicationRecord
   has_many :career_traits
   has_many :traits, through: :career_traits
-  validates :title, :summary, :detailed_interaction, :social_interaction, :salary, :entry_barriers, :remote_flexibility, :international_flexibility, :working_hour_flexibility, presence: true
+  validates :title, :summary, :detailed_description, :social_interaction, :salary, :entry_barriers, :remote_flexibility, :international_flexibility, :working_hour_flexibility, presence: true
 end
