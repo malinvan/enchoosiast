c0c1ededbe4b94a81c3faee03b8823f8ce711746
Miron Fritz <mironfritz@icloud.com>
2020-07-28 11:56:43 +0200

    fixed career show js and implemented answer create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/answers/results', to: 'answers#results'
+  get '/answers/results', to: 'answers#results', as: :results
   get '/careers/suggestions', to: 'careers#suggestions'
   resources :answers, only: [:new, :create]
   resources :careers, only: [:show]
